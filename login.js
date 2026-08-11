7c2aae3018f19c7c342331d0d27c15778bd72c90
unknown <gillioferucci@gmail.com>
2026-08-11 14:45:48 +0530

    Remove login status text on button click

--- login.js
+++ login.js
@@ -25,7 +25,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   loginBtn.addEventListener('click', () => {
-    statusEl.textContent = 'Нажмите /start в боте и перейдите по ссылке.';
     window.location.href = `https://t.me/${MOKSHA_CONFIG.telegramBotUsername}?start=auth`;
   });
 });
